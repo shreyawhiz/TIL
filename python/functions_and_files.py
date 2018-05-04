3b4f47c371531804f075ec9f91a8860507351f75
shreyawhiz <shreyabatra1992@gmail.com>
2018-05-04 14:19:58 +0530

    handling \n in readline method

--- python/functions_and_files.py
+++ python/functions_and_files.py
@@ -1,3 +1,10 @@
+# A file in Python is kind of like an old tape drive on a mainframe, or maybe a DVD player. It has
+# a READ HEAD, and you can SEEK this read head around the file to positions, then work with
+# it there. Each time you do f.seek(0), you are moving to the start of the file. Each time you do
+# f.readline(), you are reading a line from the file and moving the read head to right after the
+# \n that ends that file
+
+
 from sys import argv
 scan, file_name = argv
 
@@ -7,8 +14,13 @@ def print_file(file_name):
 def rewind_file(file_name):
 	file_name.seek(0)
 
+
+# The readline() function returns the \n that is in the file at the end of that line. This means that
+# print \n is being added to the one already returned by readline(). To change this behavior
+# simply add a , (comma) at the end of print so that it does not print its own \n.
 def print_a_line(line_number,file_name):
 	print file_name.readline()
+	# print file_name.readline(),
 
 
 print "file entered is %s" %file_name
