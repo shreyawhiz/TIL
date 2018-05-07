6098323ab39eba4b02a80db2b86b8a34d357b3ab
shreyawhiz <shreyabatra1992@gmail.com>
2018-05-07 18:52:33 +0530

    small example of hashes

---
+++ python/dictionaries_hashes.py
@@ -0,0 +1,8 @@
+stuff = {"name":"Thor", "age":1500, "nationality":"Asgardian", "brother":"Loki", "status":"God"}
+print stuff
+
+print stuff['name']
+print stuff['nationality']
+
+del stuff['brother']
+print stuff
