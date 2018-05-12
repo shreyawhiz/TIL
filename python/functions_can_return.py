95542ae04397fc1408af2bf322f589953aede51a
shreyawhiz <shreyabatra1992@gmail.com>
2018-05-12 18:03:05 +0530

    doc string in a python function

--- python/functions_can_return.py
+++ python/functions_can_return.py
@@ -1,4 +1,8 @@
 def add(a,b):
+	"""
+	This is add method
+	"""
+	# print "this is add"
 	return a+b
 
 def subtract(a,b):
@@ -18,12 +22,20 @@ print "Sum returned is : %s and subtract result returned is : %s" %(total_sum,mi
 
 print "Complicated one line script -"
 print add(divide(100,5), (divide(add(2,10), subtract(10,4))))
+print add.__doc__
 
 
 
-a = int(raw_input("Add numbers to add:"))
+# a = int(raw_input("Add numbers to add:"))
 
 
 # Every Python function returns a value; 
 # if the function ever executes a return statement, it will return that value, otherwise
 # it will return None, the Python null value.
+
+# -------------------Documenting Functions------------------------
+# You can document a Python function by giving it a doc string,
+# like i did in the add function
+# Everything between the triple quotes is the functions doc string, which documents what the function
+# does. A doc string, if it exists, must be the first thing defined in a function 
+# Can be accessed using __doc__ attribute which every python function has
